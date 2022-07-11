71c50ccded5f5fc599549f54700cde23e458eaec
YoungJae Kim <corea_kyj@naver.com>
2022-07-11 11:48:47 +0900

    change docker base image

--- make_dockerfile.py
+++ make_dockerfile.py
@@ -9,9 +9,10 @@ from enums import PythonVersionEnum
 
 CUDA_INFO_DICT = {}
 
+
 def init_json():
     global CUDA_INFO_DICT
-    with open('./jsons/cuda.json', 'r') as f:
+    with open("./jsons/cuda.json", "r") as f:
         CUDA_INFO_DICT = json.load(f)
 
 
@@ -19,79 +20,65 @@ def define_arg_parser() -> Namespace:
     init_json()
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        "--os",
+        default="ubuntu20.04",
+        choices=["ubuntu18.04", "ubuntu20.04"],
+    )
+    parser.add_argument(
         "--cuda_version",
-        choices=list(CUDA_INFO_DICT.keys()),
         default="11.3.1",
-        help="cuda version"
+        help="cuda version",
+    )
+    parser.add_argument(
+        "--cudnn_version",
+        default="8",
+        help="cudnn version",
     )
     parser.add_argument(
         "--python_version",
         default=PythonVersionEnum.DEFAULT.value,
         choices=[each.value for each in PythonVersionEnum],
-        help="python version"
+        help="python version",
     )
-    parser.add_argument(
-        "--docker",
-        action="store_true",
-        help="build and push docker"
-    )
-    parser.add_argument(
-        "--tag_name",
-        default="",
-        help="image tag"
-    )
+    parser.add_argument("--docker", action="store_true", help="build and push docker")
+    parser.add_argument("--tag_name", default="", help="image tag")
     return parser.parse_args()
 
 
 def main(args: Namespace):
     dockerfile = ""
     # Basics
-    dockerfile += dockerstrings.basic
+    if args.cuda_version in CUDA_INFO_DICT[args.os][f"cudnn{args.cudnn_version}"]:
+        os_info = args.os
+        cudnn_info = f"cudnn{args.cudnn_version}"
+        cuda_info = args.cuda_version
+        dockerfile += dockerstrings.basic.format(
+            DOCKER_BASE_IMAGE=f"{cuda_info}-{cudnn_info}-devel-{os_info}"
+        )
+    else:
+        # Only Support Ubuntu
+        os_info = args.os[:6] + ":" + args.os[6:]
+        dockerfile += dockerstrings.basic.format(
+            DOCKER_BASE_IMAGE=os_info
+        )
+        
     # Ubuntu Package
     dockerfile += dockerstrings.ubuntu_package
-    # CUDA
-    cuda_json = CUDA_INFO_DICT[args.cuda_version]
-    dockerfile += dockerstrings.cuda.format(
-        CUDA_DOT_VERSION=cuda_json["CUDA_DOT_VERSION"],
-        CUDA_HYPHEN_VERSION=cuda_json["CUDA_HYPHEN_VERSION"],
-        CUDA_VERSION=cuda_json["CUDA_VERSION"],
-        NV_CUDA_CUDART_VERSION=cuda_json["NV_CUDA_CUDART_VERSION"],
-        NV_CUDA_COMPAT_PACKAGE=cuda_json["NV_CUDA_COMPAT_PACKAGE"],
-        NV_CUDA_LIB_VERSION=cuda_json["NV_CUDA_LIB_VERSION"],
-        NV_NVTX_VERSION=cuda_json["NV_NVTX_VERSION"],
-        NV_LIBNPP_VERSION=cuda_json["NV_LIBNPP_VERSION"],
-        NV_LIBCUSPARSE_VERSION=cuda_json["NV_LIBCUSPARSE_VERSION"],
-        NV_LIBCUBLAS_PACKAGE_NAME=cuda_json["NV_LIBCUBLAS_PACKAGE_NAME"],
-        NV_LIBCUBLAS_VERSION=cuda_json["NV_LIBCUBLAS_VERSION"],
-        NV_LIBNCCL_PACKAGE_NAME=cuda_json["NV_LIBNCCL_PACKAGE_NAME"],
-        NV_LIBNCCL_PACKAGE_VERSION=cuda_json["NV_LIBNCCL_PACKAGE_VERSION"],
-        NCCL_VERSION=cuda_json["NCCL_VERSION"],
-        NV_CUDA_CUDART_DEV_VERSION=cuda_json["NV_CUDA_CUDART_DEV_VERSION"],
-        NV_NVML_DEV_VERSION=cuda_json["NV_NVML_DEV_VERSION"],
-        NV_LIBCUSPARSE_DEV_VERSION=cuda_json["NV_LIBCUSPARSE_DEV_VERSION"],
-        NV_LIBNPP_DEV_VERSION=cuda_json["NV_LIBNPP_DEV_VERSION"],
-        NV_LIBCUBLAS_DEV_VERSION=cuda_json["NV_LIBCUBLAS_DEV_VERSION"],
-        NV_LIBCUBLAS_DEV_PACKAGE_NAME=cuda_json["NV_LIBCUBLAS_DEV_PACKAGE_NAME"],
-        NV_LIBNCCL_DEV_PACKAGE_NAME=cuda_json["NV_LIBNCCL_DEV_PACKAGE_NAME"],
-        NV_LIBNCCL_DEV_PACKAGE_VERSION=cuda_json["NV_LIBNCCL_DEV_PACKAGE_VERSION"],
-        NV_CUDNN_VERSION=cuda_json["NV_CUDNN_VERSION"],
-        NV_CUDNN_PACKAGE_NAME=cuda_json["NV_CUDNN_PACKAGE_NAME"],
-    )
     # Python Mamba
     dockerfile += dockerstrings.python_mamba.format(PYTHON_VERSION=args.python_version)
     # Dev Tools
     dockerfile += dockerstrings.dev_tools
     # Start shell
     dockerfile += dockerstrings.start_shell
-    with open('Dockerfile', 'w') as f:
+    with open("Dockerfile", "w") as f:
         f.write(dockerfile)
     if args.docker:
         if not args.tag_name:
-            print('[ERROR]: tag name is need')
+            print("[ERROR]: tag name is need")
             exit(1)
         build_command = f"docker build -t {args.tag_name} ."
         build_flag = False
-        print('Build :', build_command)
+        print("Build :", build_command)
         with Popen(build_command, shell=True) as p:
             try:
                 p.wait()
@@ -102,7 +89,7 @@ def main(args: Namespace):
 
         if build_flag:
             push_command = f"docker push {args.tag_name}"
-            print('Push :', push_command)
+            print("Push :", push_command)
             with Popen(push_command, shell=True) as p:
                 try:
                     p.wait()
@@ -111,6 +98,6 @@ def main(args: Namespace):
                     p.wait()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = define_arg_parser()
     main(args)
